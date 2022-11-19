463cf98973de58c6669c1a5274203ae9e6603c43
Szabó Dorottya <szabo.dori94@gmail.com>
2022-11-19 15:17:06 +0100

    route

--- hy22-frontend-418/src/App.js
+++ hy22-frontend-418/src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import { Sidebar } from './sidebar/Sidebar';
+import React from 'react';
+import styles from './App.module.scss';
+import { Home } from './home/Home';
+import { Routes, Route } from "react-router-dom"
 
 function App() {
-  return (
-    <div className="container">
-      <div className='side-container'>
-        <Sidebar></Sidebar>
-      </div>
-      <div className='main-container'>
-        test
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.app}>
+			<Routes>
+				<Route path="/" element={ <Home/> } />
+				<Route path="test" element={ <Home/> } />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
